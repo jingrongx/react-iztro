0f0a15ca07b8a554f762bb2364c8686c3c980388
Xu, Jingrong <xujingrong168@qq.com>
2026-04-09 09:08:17 +0800

    Add Android support and build APK

---
+++ demo/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iztro.ai',
+  appName: '紫微斗数AI解读',
+  webDir: 'dist'
+};
+
+export default config;
